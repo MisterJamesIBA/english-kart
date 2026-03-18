9bf1a6527fa237498d1a4294dfe7765c85aaa64e
MisterJamesIBA <mrjamesiba@gmail.com>
2026-03-18 19:19:29 +0900

    change

---
+++ js/revisedAuto.js
@@ -0,0 +1,69 @@
+
+
+
+
+window.onload = function () {
+
+    // canvas 
+    let canvas = document.querySelector("#canvas");
+
+    // show and hide boxes
+    let startBox = document.querySelector("#startBox");
+    let numberBox = document.querySelector("#numberBox");
+    let charBox = document.querySelector("#charBox");
+    let diceBox = document.querySelector("#diceBox");
+    let questBox = document.querySelector("#questBox");
+    let itemBox = document.querySelector("#itemBox");
+    let endBox = document.querySelector("#endBox");
+
+    // buttons
+    let startGameBtn = document.querySelector("#startGameBtn");
+    let nmbrEntrBtn = document.querySelector("#nmbrEntrBtn");
+    let charEntrBtn = document.querySelector("#charEntrBtn");
+
+    // roll buttons
+    let rollBtn = document.querySelector("#rollBtn");
+    let rollNextBtn = document.querySelector("#rollNextBtn");
+
+    //quest
+    let checkQuestBtn = document.querySelector("#checkQuestBtn");
+    let nextQuestBtn = document.querySelector("#nextQuestBtn");
+
+    // item
+    let getItemBtn = document.querySelector("#getItemBtn");
+    let nextItemBtn = document.querySelector("#nextItemBtn");
+
+
+    // start button
+    startGameBtn.addEventListener("click", () => {
+
+    });
+
+    // enter number button
+    nmbrEntrBtn.addEventListener("click", () => {
+
+    });
+
+    // pick charcater button
+    charEntrBtn.addEventListener("click", () => {
+
+    });
+
+
+    // roll dice button
+    rollBtn.addEventListener("click", () => {
+
+    });
+
+
+    // dive next button
+    rollNextBtn.addEventListener("click", () => {
+
+    });
+
+    // check answer for queston
+
+    // question next button
+
+    // start game
+}
